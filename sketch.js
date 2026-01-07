cecb49f33d08d6ebd0db840cef8497183fedeec1
Cybergman-droid <jasongakpo20@gmail.com>
2026-01-07 13:01:02 +0000

    started on the tic tac toe game

--- sketch.js
+++ sketch.js
@@ -1,8 +1,32 @@
-function setup() {
-    createCanvas(400, 400);
+
+let board = [
+['O','X','O'],
+['X','X','O'],
+['X','O','X'],
+];
+
+let player1 = 'X'
+let player2 = 'O'
+
+function setup(){
+    createCanvas(400,400)
 }
 
-function draw() {
-    background(200, 50, 200);
-    triangle(30, 75, 58, 20, 86, 75);
+function draw(){
+    background(220)
+    let w = width / 3
+    let h  = height / 3
+    for (let i = 0; i < 3;i++){
+        for(let j = 0; j < 3; j++){
+            let x = w * i
+            let y = h * j
+            let spot = board[i][j]
+            textSize(32)
+            if (spot == player2){
+                noFill();
+            } else if(spot == player1){
+
+            }
+        }
+    }
 }
